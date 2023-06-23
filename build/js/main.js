7c4fd673ee2e0388beed26e26d22a006e42a97dc
Hopeson <hopesonogaga@gmail.com>
2023-06-23 13:05:45 +0100

    js fucntion

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,39 @@
-const slidesContainer = document.querySelector(".carosel");
-const slide = document.querySelectorAll(".slide");
+const slidesContainer = document.querySelector(".carousel");
+const slides = document.querySelectorAll(".slide");
 const nextButton = document.querySelector(".nav_left");
 const prevButton = document.querySelector(".nav_right");
+const increment = document.querySelector(".increment");
+const decrement = document.querySelector(".decrement");
+const numberBtn = document.querySelector(".number_btn");
+const cart_image = document.querySelector(".cart_image");
+const check_div = document.querySelector(".check_div");
 
+let incNumber = 0;
 
+cart_image.addEventListener("click", function () {
+  check_div.classList.toggle("hidden");
+});
+// btn functions
+increment.addEventListener("click", function () {
+  incNumber = incNumber + 1;
+  console.log(incNumber);
+  numberBtn.textContent = incNumber;
+});
 
+decrement.addEventListener("click", function () {
+  if (incNumber > 0) {
+    incNumber -= 1;
+  }
+  console.log(incNumber);
+  numberBtn.textContent = incNumber;
+});
 
 nextButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft += slideWidth;
-  });
-  
-  prevButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
-  });
+  const slideWidth = slides[0].clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slides[0].clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
